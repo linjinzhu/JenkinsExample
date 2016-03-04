ac1fa82f0784befb6b02ea273afbd27e221f1b31
linjinzhu <linjinzhu@hotmail.com>
2016-03-04 18:21:47 +0800

    changes:update jenkins script.

--- jenkins/scripts/update-bundle.sh
+++ jenkins/scripts/update-bundle.sh
@@ -7,8 +7,3 @@ if [ ! -z "$BUNDLE_IDENTIFIER" ]; then
   /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $BUNDLE_IDENTIFIER" "$INFO_PLIST"
   echo "Set CFBundleIdentifier to $BUNDLE_IDENTIFIER"
 fi
-
-if [ ! -z "$BUNDLE_DISPLAY_NAME" ]; then
-  /usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName $BUNDLE_DISPLAY_NAME" "$INFO_PLIST"
-  echo "Set CFBundleDisplayName to $BUNDLE_DISPLAY_NAME"
-fi
